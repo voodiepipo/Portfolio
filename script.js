9a81f9b77e3433b088a345e01bf819682538660c
voodiepipo <159997939+voodiepipo@users.noreply.github.com>
2026-03-17 11:56:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  const revealPoint = 100;
+
+  reveals.forEach(element => {
+    const elementTop = element.getBoundingClientRect().top;
+    if (elementTop < windowHeight - revealPoint) {
+      element.classList.add('active');
+    }
+  });
+}
+
+revealOnScroll();
+window.addEventListener('scroll', revealOnScroll);
+
+// หยุด floating เมื่อเริ่ม scroll
+const profileImg = document.querySelector('.profile-img');
+
+
+window.addEventListener('scroll', () => {
+  if (profileImg) {
+    profileImg.style.animation = 'none';
+  }
+});
